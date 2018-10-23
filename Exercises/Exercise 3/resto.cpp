56b59a99cbbadf140442662ffbf4ebe07f403e5a
Dimitko <penkov.dimit@gmail.com>
2018-10-23 19:28:48 +0300

    upgraded the resto.cpp solution to work with two cycles

--- Exercises/Exercise 3/resto.cpp
+++ Exercises/Exercise 3/resto.cpp
@@ -9,41 +9,30 @@ int main()
     int ednolevki=0, dvulevki=0, petolevki=0;
 
     int currentSum = 0;
+    int counter = 0;
 
-    while(currentSum <= sum)
+    while(currentSum < sum)
     {
-        while(currentSum <= sum)
+        while(currentSum < sum)
         {
-            while(currentSum <= sum)
-            {
-                ednolevki ++;
-                currentSum += 1;
-                if(currentSum == sum)
-                {
-                    cout<<"broi ednolevki = "<<ednolevki<<"\nbroi dvulevki = "<<dvulevki<<"\nbroi petolevki = "<<petolevki<<endl;
-                    cout<<"@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@\n";
-                }
-            }
-            currentSum -= ednolevki;
+            ednolevki = sum - currentSum;
+            cout<<"broi ednolevki = "<<ednolevki<<"\nbroi dvulevki = "<<dvulevki<<"\nbroi petolevki = "<<petolevki<<endl;
+            cout<<"***************************************************************\n";
+            counter ++;
             ednolevki = 0;
             dvulevki ++;
             currentSum += 2;
-            if(currentSum == sum)
-            {
-                cout<<"broi ednolevki = "<<ednolevki<<"\nbroi dvulevki = "<<dvulevki<<"\nbroi petolevki = "<<petolevki<<endl;
-                cout<<"@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@\n";
-            }
         }
+        cout<<"broi ednolevki = "<<ednolevki<<"\nbroi dvulevki = "<<dvulevki<<"\nbroi petolevki = "<<petolevki<<endl;
+        cout<<"***************************************************************\n";
+        counter ++;
         currentSum -= dvulevki * 2;
         dvulevki = 0;
         petolevki ++;
         currentSum += 5;
-        if(currentSum == sum)
-        {
-            cout<<"broi ednolevki = "<<ednolevki<<"\nbroi dvulevki = "<<dvulevki<<"\nbroi petolevki = "<<petolevki<<endl;
-            cout<<"@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@\n";
-        }
     }
+
+    cout <<"Broi vazmojni kombinacii = "<<counter<<endl;
 
     return 0;
 }
