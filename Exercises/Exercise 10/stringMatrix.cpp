22f0f691754a325932ad102ccd05c7c263a9b294
Dimitko <penkov.dimit@gmail.com>
2018-12-07 12:55:06 +0200

    Added some correction and descriptions

--- Exercises/Exercise 10/stringMatrix.cpp
+++ Exercises/Exercise 10/stringMatrix.cpp
@@ -43,23 +43,36 @@ int printLongestStrByRow(char strMat[100][100][100], int row, int sizeX, int siz
     return currentMax;
 }
 
-int* printLongestStrEveryRow(char strMat[100][100][100], int sizeX, int sizeY)
+int* printLongestStrEveryRowWrong(char strMat[100][100][100], int sizeX, int sizeY)
 {
-    int arr[100];
+    int result[100];
     for(int i = 0; i<sizeX; i++)
     {
-        arr[i] = printLongestStrByRow(strMat, i, sizeX, sizeY);
-        cout<<setw(3)<<arr[i]<<endl;
+        result[i] = printLongestStrByRow(strMat, i, sizeX, sizeY);
+        cout<<setw(3)<<result[i]<<endl;
     }
-    return arr;
+    return result; /// warning: address of local variable 'result' returned
+                /// We return the address of the array declared in the function and then free the memory.
+                /// WRONG!!!
+}
+
+void printLongestStrEveryRow(char strMat[100][100][100], int sizeX, int sizeY, int* result) /// We pass the array we want to write in as a parameter.
+                                                                                            /// CORRECT!!!
+{
+    for(int i = 0; i<sizeX; i++)
+    {
+        result[i] = printLongestStrByRow(strMat, i, sizeX, sizeY);
+        cout<<setw(3)<<result[i]<<endl;
+    }
 }
 
 void reverseStr(char str[100]){
     int sizeS = strSize(str);
     for(int i = 0; i<sizeS/2; i++){
-        swap(str[i], str[sizeS-i-1]); /// We forgot to decrease the value with 1 in str[sizeS - i - 1]. That's the program didn't work as expected.
+        swap(str[i], str[sizeS-i-1]); /// We had forgotten to decrease the value with 1 in str[sizeS - i - 1].
+                                      /// We used to put the '\0' sign on the first position of the string.
+                                      /// That's why the program didn't work as expected.
     }
-//    cout<<"reversed( "<<str<<" )"<<endl;
 }
 
 void reverseStrMat(char strMat[100][100][100], int sizeX, int sizeY){
@@ -72,16 +85,19 @@ void reverseStrMat(char strMat[100][100][100], int sizeX, int sizeY){
 
 int main()
 {
-    char strM[100][100][100] = {{"asd", "123", "Hello!"}, {"second line", "next element", "world"}};
-    printStrMat(strM, 2, 3);
-    int* arr;
-    arr = printLongestStrEveryRow(strM, 2, 3);
-//    for(int i = 0; i<2; i++){
-//        cout<<i<<endl;
-//        cout<<arr[0]<<endl;
-//    }
-    cout<<printLongestStrByRow(strM, 1, 2, 3)<<endl;
+    char strM[100][100][100] = {{"asd", "123", "Hello!"}, {"second line", "next element", "world"}, {"third line", "more elements", "I'm a string!"}};
+    printStrMat(strM, 3, 3);
+    int arr[100];
+    printLongestStrEveryRow(strM, 3, 3, arr);
+    for(int i = 0; i<3; i++){
+        cout<<"result_array["<<i<<"] = "<<arr[i]<<endl;
+    }
+    int* arr2 = printLongestStrEveryRowWrong(strM, 3, 3);
+    for(int i = 0; i<3; i++){
+        cout<<"result_array["<<i<<"] = "<<arr2[i]<<endl;
+    }
+    cout<<printLongestStrByRow(strM, 1, 3, 3)<<endl;
 
-    reverseStrMat(strM, 2, 3);
-    printStrMat(strM, 2, 3);
+    reverseStrMat(strM, 3, 3);
+    printStrMat(strM, 3, 3);
 }
