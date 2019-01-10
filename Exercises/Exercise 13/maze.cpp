20ab7fee5304e37d2e40b473b6a6f6f5a1171100
Dimitko <penkov.dimit@gmail.com>
2019-01-11 00:18:33 +0200

    Find and print way in maze

--- Exercises/Exercise 13/maze.cpp
+++ Exercises/Exercise 13/maze.cpp
@@ -1,77 +1,191 @@
 #include<iostream>
+#include<iomanip>
 using namespace std;
 
-int mazeM[8][8] = {
-{0,1,0,0,0,1,0,0},
-{0,1,0,0,0,1,0,0},
-{0,1,0,0,1,1,1,1},
-{0,1,0,1,0,0,0,0},
-{0,1,0,1,1,1,0,0},
-{0,0,0,0,0,0,0,0},
-{0,1,0,1,1,1,0,1},
-{0,1,0,1,1,0,0,0}
+int mazeM[8][8] =
+{
+    {0,1,0,0,0,1,0,0},
+    {0,1,0,0,0,1,0,0},
+    {0,1,0,0,1,1,1,1},
+    {0,1,0,1,0,0,0,0},
+    {0,1,0,1,1,1,0,0},
+    {0,0,0,0,0,0,0,0},
+    {0,1,0,1,1,1,0,1},
+    {0,1,0,1,1,0,0,0}
 };
 
-bool walkable(int maze[8][8], int X, int Y){
-    if(X<0 || Y<0 || X>7 || Y>7 || maze[X][Y] != 0){
+bool walkable(int maze[8][8], int X, int Y)
+{
+    if(X<0 || Y<0 || X>7 || Y>7 || maze[X][Y] != 0)
+    {
         return false;
     }
     return true;
 }
 
-bool way(int maze[8][8], int startX, int startY, int endX, int endY){
+bool way(int maze[8][8], int startX, int startY, int endX, int endY)
+{
     maze[startX][startY] = 2;
     bool result = false;
-    if(startX == endX && startY == endY){
+
+    if(startX == endX && startY == endY)
+    {
         return true;
     }
-    if(walkable(maze, startX + 1, startY)){
+    if(walkable(maze, startX + 1, startY))
+    {
         result = result || way(maze, startX + 1, startY, endX, endY);
     }
-    if(walkable(maze, startX, startY + 1)){
+    if(walkable(maze, startX, startY + 1))
+    {
         result = result || way(maze, startX, startY + 1, endX, endY);
     }
-    if(walkable(maze, startX - 1, startY)){
+    if(walkable(maze, startX - 1, startY))
+    {
         result = result || way(maze, startX - 1, startY, endX, endY);
     }
-    if(walkable(maze, startX, startY - 1)){
+    if(walkable(maze, startX, startY - 1))
+    {
         result = result || way(maze, startX, startY - 1, endX, endY);
     }
     return result;
 }
 
-int** findWay(int maze[8][8], int startX, int startY, int endX, int endY, int* way[2], int steps){
+unsigned int findWay(int maze[8][8], int startX, int startY, int endX, int endY, int way[][2], int steps)
+{
     maze[startX][startY] = 2;
     way[steps][0] = startX;
     way[steps][1] = startY;
-    int** result = nullptr;
+    unsigned int result1 = -1, result2 = -1, result3 = -1, result4 = -1;
+    cout<<"X = "<<startX<<", Y = "<<startY<<endl;
 
-    if(startX == endX && startY == endY){
-        return way;
+    if(startX == endX && startY == endY)
+    {
+        return steps;
     }
-    if(walkable(maze, startX + 1, startY)){
-        result = findWay(maze, startX + 1, startY, endX, endY, way, steps+1);
+    if(walkable(maze, startX + 1, startY))
+    {
+//    cout<<"X = "<<startX<<", Y = "<<startY<<endl;
+        result1 = findWay(maze, startX + 1, startY, endX, endY, way, steps + 1);
+//        if(result) return result;
+    }
+    if(walkable(maze, startX, startY + 1))
+    {
+//    cout<<"X = "<<startX<<", Y = "<<startY<<endl;
+        result2 = findWay(maze, startX, startY + 1, endX, endY, way, steps + 1);
+//        if(result) return result;
+    }
+    if(walkable(maze, startX - 1, startY))
+    {
+//    cout<<"X = "<<startX<<", Y = "<<startY<<endl;
+        result3 = findWay(maze, startX - 1, startY, endX, endY, way, steps + 1);
+//        if(result) return result;
+    }
+    if(walkable(maze, startX, startY - 1))
+    {
+//    cout<<"X = "<<startX<<", Y = "<<startY<<endl;
+        result4 = findWay(maze, startX, startY - 1, endX, endY, way, steps + 1);
+//        if(result) return result;
+    }
+    return min(result1, min(result2, min(result3, result4)));
+}
+
+/// In the maze we are going to mark the path we took to get to target position.
+/// In each position we are going to say where we came from to get there.
+/// 10 - up, 11 - left, 12 - down, 13 - right
+bool findWay2(int maze[8][8], int startX, int startY, int endX, int endY)
+{
+    bool result = false;
+    if(startX == endX && startY == endY)
+    {
+        return true;
+    }
+    if(walkable(maze, startX + 1, startY))
+    {
+        maze[startX + 1][startY] = 10;
+        result = findWay2(maze, startX + 1, startY, endX, endY);
         if(result) return result;
     }
-    if(walkable(maze, startX, startY + 1)){
-        result = findWay(maze, startX, startY + 1, endX, endY, way, steps+1);
+    if(walkable(maze, startX, startY + 1))
+    {
+        maze[startX][startY + 1] = 11;
+        result = findWay2(maze, startX, startY + 1, endX, endY);
         if(result) return result;
     }
-    if(walkable(maze, startX - 1, startY)){
-        result = findWay(maze, startX - 1, startY, endX, endY, way, steps+1);
+    if(walkable(maze, startX - 1, startY))
+    {
+        maze[startX - 1][startY] = 12;
+        result = findWay2(maze, startX - 1, startY, endX, endY);
         if(result) return result;
     }
-    if(walkable(maze, startX, startY - 1)){
-        result = findWay(maze, startX, startY - 1, endX, endY, way, steps+1);
+    if(walkable(maze, startX, startY - 1))
+    {
+        maze[startX][startY - 1] = 13;
+        result = findWay2(maze, startX, startY - 1, endX, endY);
         if(result) return result;
     }
     return result;
 }
 
-int main(){
-    cout<<way(mazeM, 0, 0, 1, 4);
-    int foundWay[100][2];
-    for(int i = 0; i<100; i++){
-        cout<<foundWay[i][0]<<", "<<foundWay[i][1]<<endl;
+void printWay(int maze[8][8], int startX, int startY, int endX, int endY)
+{
+    cout<<endX<<' '<<endY<<endl;
+    if(endX != startX || endY != startY)
+    {
+        if(maze[endX][endY] == 10)
+        {
+            printWay(maze, startX, startY, endX - 1, endY);
+        }
+        if(maze[endX][endY] == 11)
+        {
+            printWay(maze, startX, startY, endX, endY - 1);
+        }
+        if(maze[endX][endY] == 12)
+        {
+            printWay(maze, startX, startY, endX + 1, endY);
+        }
+        if(maze[endX][endY] == 13)
+        {
+            printWay(maze, startX, startY, endX, endY + 1);
+        }
     }
+}
+
+bool findAndPrintWay(int maze[8][8], int startX, int startY, int endX, int endY){
+    bool result = findWay2(maze, startX, startY, endX, endY);
+    if(result){
+        printWay(maze, startX, startY, endX, endY);
+    }
+    return result;
+}
+
+void printMaze(int maze[8][8]){
+    for(int i = 0; i<8; i++){
+        for(int j = 0; j<8; j++){
+            cout<<setw(3)<<maze[i][j];
+        }
+        cout<<endl;
+    }
+}
+
+int main()
+{
+//    cout<<way(mazeM, 0, 0, 1, 4)<<endl;
+
+//    int foundWay[100][2];
+//    for(int i = 0; i<100; i++)
+//    {
+//        foundWay[i][0] = 0;
+//        foundWay[i][1] = 0;
+//    }
+//    int steps = findWay(mazeM, 0, 0, 0, 1, foundWay, 0);
+//    cout<<steps<<endl;
+//    for(int i = 0; i<= 99; i++)
+//    {
+//        cout<<foundWay[i][0]<<", "<<foundWay[i][1]<<endl;
+//    }
+
+    cout<<findAndPrintWay(mazeM, 0, 0, 0, 1)<<endl;
+    printMaze(mazeM);
+
 }
--- Exercises/Exercise 13/ourQuicksort.cpp
+++ Exercises/Exercise 13/ourQuicksort.cpp
@@ -1,117 +1,52 @@
-<<<<<<< HEAD
 #include<iostream>
 using namespace std;
 
-void swap2(double& num1, double& num2){
-    double num3;
-    num3 = num1;
+void swap1(double& num1, double& num2)
+{
+    double num3 = num1;
     num1 = num2;
     num2 = num3;
 }
 
-int partition1(int lo, int hi, double* arr){
+int partition1(double* arr, int lo, int hi)
+{
     double pivot = arr[hi];
     int i = lo - 1;
-    for(int j = lo; j<hi; j++){
-        if(pivot > arr[j]){
+
+    for(int j = lo; j<hi; j++)
+    {
+        if(pivot > arr[j])
+        {
             i++;
-            swap(arr[i], arr[j]);
+            swap1(arr[i], arr[j]);
         }
     }
-    swap2(arr[i+1], arr[hi]);
+    swap1(arr[i+1], arr[hi]);
     return i+1;
 }
 
-void quicksort(int lo, int hi, double* arr ){
-    while(lo<hi){
-        int sorted = partition1(lo, hi, arr);
-        quicksort(lo, sorted-1, arr);
-        quicksort(sorted+1, hi, arr);
-    }
-}
-
-int main(){
-    double arr[] = {40, 23, 5, -52, 43, 10, -0.5, 34.56};
-    quicksort(0, 6, arr);
-    for(int i = 0; i<8; i++){
-        cout<<arr[i]<<' ';
-    }
-}
-
-=======
-/* C implementation QuickSort */
-#include<stdio.h>
-#include<iostream>
-
-// A utility function to swap two elements
-void swap(int& a, int& b)
+void quicksort(double* arr, int lo, int hi )
 {
-    int t = a;
-    a = b;
-    b = t;
-}
-
-/* This function takes last element as pivot, places
-   the pivot element at its correct position in sorted
-    array, and places all smaller (smaller than pivot)
-   to left of pivot and all greater elements to right
-   of pivot */
-int partition (int arr[], int low, int high)
-{
-    int pivot = arr[high];    // pivot
-    int i = (low - 1);  // Index of smaller element
-
-    for (int j = low; j < high; j++)
+    if(lo<hi) /// We had accidentally used while instead of an if
     {
-        // If current element is smaller than or
-        // equal to pivot
-        if (arr[j] < pivot)
-        {
-            i++;    // increment index of smaller element
-            swap(arr[i], arr[j]);
-        }
-    }
-    swap(arr[i + 1], arr[high]);
-    return (i + 1);
-}
+        int sorted = partition1(arr, lo, hi);
 
-/* The main function that implements QuickSort
- arr[] --> Array to be sorted,
-  low  --> Starting index,
-  high  --> Ending index */
-void quickSort(int arr[], int low, int high)
-{
-    if (low < high)
-    {
-        /* pi is partitioning index, arr[pi] is now
-           at right place */
-        int pi = partition(arr, low, high);
+//        cout<<"lo = "<<lo<<", hi = "<<hi<<", sorted element = "<< arr[sorted]<<endl;
 
-        // Separately sort elements before
-        // partition and after partition
-        quickSort(arr, low, pi - 1);
-        quickSort(arr, pi + 1, high);
+        quicksort(arr, lo, sorted-1);
+        quicksort(arr, sorted+1, hi);
     }
 }
 
-/* Function to print an array */
-void printArray(int arr[], int size)
-{
-    int i;
-    for (i=0; i < size; i++)
-        printf("%d ", arr[i]);
-    printf("\n");
-}
-
-// Driver program to test above functions
 int main()
 {
-    int arr[] = {10, 7, 8, 9, 1, 5,43,65234,1,-543,123,-32,0,213,0};
-//    int arr[] = {10,9,8,7,6,5,4,3,2,1};
-    int n = sizeof(arr)/sizeof(arr[0]);
-    quickSort(arr, 0, n-1);
-    printf("Sorted array: \n");
-    printArray(arr, n);
-    return 0;
+    double arr[] = {40, 23, 5, -52, 43, 10, -0.5, 34.56};
+    double arr1[] = {450, 52.7, 0, -65, 3.9, -75.9, 24, 64, 0.5, -23};
+    double arr2[] = {3, 4, 1, 5, 2, 6, 3.4, -5.3, -4, 3.5};
+    int n = sizeof(arr1)/sizeof(arr1[0]);
+    quicksort(arr1, 0, n-1);
+    for(int i = 0; i<n; i++)
+    {
+        cout<<arr1[i]<<", ";
+    }
 }
->>>>>>> 92b4d587cab838df0e00875f391d54cb30896cc8
